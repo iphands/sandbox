fc31c4f817fc38db1a6e9ecdd9d0df634316dea5
Ian Page Hands <iphands@gmail.com>
2010-07-20 21:48:40 -0400

    figuring ptrace out

--- ptrace-test.c
+++ ptrace-test.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ptrace.h>
+#include <sys/types.h>
 
-int main()
+
+int main(int argc, char ** argv)
 {
+
+  if (argc < 2) {
+    printf("Please pass in a pid number\n");
+    return(1);
+  }
   
+  pid_t pid = atoi(argv[1]);
+  printf("Attaching to pid %d\n", (int)pid);
+
+  long data = ptrace(PTRACE_ATTACH, pid, 0, NULL);
+  printf("attach return: %d\n", (int)data);
+  if (data == -1) {
+    printf("Failed to attach!\n");
+    return(1);
+  }
+
+  for (int i = 0; i < 100; i++) {
+    data = ptrace(PTRACE_PEEKUSER, pid, i, NULL);
+    if ( (data == -1) || (data == 0) ) {
+      continue;
+    }
+    printf("int[%d]: %ld\n", i, data);
+    printf("hex[%d]: %x\n", i, (unsigned int) data);
+  }
+
+  ptrace(PTRACE_DETACH, pid, NULL, NULL);
+
   return(0);
 }
--- stack-pause.c
+++ stack-pause.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main()
 {
   int my_int = 1234567890;
-  printf("%d on the stack, pausing...\n", my_int);
-  getchar();
+  char * string = "Hello World";
+  printf("My pid is %d\n", (int)getpid());
+  printf("%s on the stack (%x)\n", string, (unsigned int) &string);
+  printf("%d on the stack (%x)\n", my_int, (unsigned int) &my_int);
+  printf("entering pause loop...\n");
+  while(1) {
+    usleep(1000 * 250);
+  }
+
   return(0);
 }

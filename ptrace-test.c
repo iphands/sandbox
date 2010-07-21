c60ead84cc5405caada0ac8edebe9906e5f6dbc7
Ian Page Hands <iphands@gmail.com>
2010-07-20 22:31:20 -0400

    figuring ptrace out

--- ptrace-test.c
+++ ptrace-test.c
@@ -2,7 +2,34 @@
 #include <stdlib.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
+#include <sys/user.h>
 
+void print_ur_struct(struct user_regs_struct regs);
+
+void print_ur_struct(struct user_regs_struct regs)
+{
+  printf("\nstruct user_regs_struct\n");
+  printf("{\n");
+  printf("  long int ebx; (0x%x)\n", (unsigned int) regs.ebx);
+  printf("  long int ecx; (0x%x)\n", (unsigned int) regs.ecx);
+  printf("  long int edx; (0x%x)\n", (unsigned int) regs.edx);
+  printf("  long int esi; (0x%x)\n", (unsigned int) regs.esi);
+  printf("  long int edi; (0x%x)\n", (unsigned int) regs.edi);
+  printf("  long int ebp; (0x%x)\n", (unsigned int) regs.ebp);
+  printf("  long int eax; (0x%x)\n", (unsigned int) regs.eax);
+  printf("  long int xds; (0x%x)\n", (unsigned int) regs.xds);
+  printf("  long int xes; (0x%x)\n", (unsigned int) regs.xes);
+  printf("  long int xfs; (0x%x)\n", (unsigned int) regs.xfs);
+  printf("  long int xgs; (0x%x)\n", (unsigned int) regs.xgs);
+  printf("  long int orig_eax; (0x%x)\n", (unsigned int) regs.orig_eax);
+  printf("  long int eip; (0x%x)\n", (unsigned int) regs.eip);
+  printf("  long int xcs; (0x%x)\n", (unsigned int) regs.xcs);
+  printf("  long int eflags; (0x%x)\n", (unsigned int) regs.eflags);
+  printf("  long int esp; (0x%x)\n", (unsigned int) regs.esp);
+  printf("  long int xss; (0x%x)\n", (unsigned int) regs.xss);
+  printf("};\n\n");
+  return;
+}
 
 int main(int argc, char ** argv)
 {
@@ -22,8 +49,12 @@ int main(int argc, char ** argv)
     return(1);
   }
 
-  for (int i = 0; i < 100; i++) {
-    data = ptrace(PTRACE_PEEKUSER, pid, i, NULL);
+  struct user_regs_struct regs;
+  ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+  print_ur_struct(regs);
+
+  for (int i = 0; i < 1000; i++) {
+    data = ptrace(PTRACE_PEEKDATA, pid, i, NULL);
     if ( (data == -1) || (data == 0) ) {
       continue;
     }
